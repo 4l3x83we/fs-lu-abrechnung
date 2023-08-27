5b511bcb14c5c14ce086779069564b178e3e2cd7
Alexander Guthmann <4l3x83we.dev@gmail.com>
2023-08-27 05:34:12 +0200

    change components

--- resources/views/components/custom/forms/checkbox-radio.blade.php
+++ resources/views/components/custom/forms/checkbox-radio.blade.php
@@ -1,5 +1,5 @@
 @error($id ?? '')
-<div class="flex items-center mr-4">
+<div class="flex items-center mr-4 mb-2 xl:mb-0 last:mb-0">
     <input {{ $attributes->merge(['type' => 'checkbox', 'id' => $id ?? '', 'value' => $id ?? '', 'class' => 'w-4 h-4 text-gray-600 bg-gray-100 border-red-300 focus:ring-gray-500 dark:focus:ring-gray-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-red-600'])  }}>
     <label for="{{ $id ?? '' }}" class="ml-2 text-sm font-medium text-red-600 dark:text-red-500">{{ $text ?? '' }}</label>
 </div>
@@ -7,7 +7,7 @@
     {{ $message }}
 </span>
 @else
-<div class="flex items-center mr-4">
+<div class="flex items-center mr-4 mb-2 xl:mb-0 last:mb-0">
     <input {{ $attributes->merge(['type' => 'checkbox', 'id' => $id ?? '', 'value' => $id ?? '', 'class' => 'w-4 h-4 text-gray-600 bg-gray-100 border-gray-300 focus:ring-gray-500 dark:focus:ring-gray-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'])  }}>
     <label for="{{ $id ?? '' }}" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">{{ $text ?? '' }}</label>
 </div>
